06e2b13724940ed0309a4d288f00f12003560890
fvucemilo <fvucemilo@tvz.hr>
2021-01-27 23:26:37 +0100

    second commit

--- app/src/main/java/com/fvucemilo/trello/SignInActivity.kt
+++ app/src/main/java/com/fvucemilo/trello/SignInActivity.kt
@@ -1,10 +1,14 @@
 package com.fvucemilo.trello
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.graphics.Color
+import android.net.Uri
 import android.os.Bundle
 import android.view.WindowManager
-import com.fvucemilo.trello.R
+import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_sign_in.*
+import java.util.*
 
 class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +19,11 @@ class SignInActivity : AppCompatActivity() {
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
+
+        btn_sign_in.setOnClickListener {
+            // Launch the sign in screen.
+            showUrl()
+        }
 
         setupActionBar()
     }
@@ -34,5 +43,18 @@ class SignInActivity : AppCompatActivity() {
         toolbar_sign_in_activity.setNavigationOnClickListener {
             onBackPressed()
         }
+    }
+
+    private fun showUrl() {
+        val uri: Uri = Uri.parse("https://www.tvz.hr")
+        startActivity(Intent(Intent.ACTION_VIEW, uri))
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        val rnd = Random()
+        val color = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256))
+        val signInActivity = findViewById<LinearLayout>(R.id.test)
+        signInActivity.setBackgroundColor(color)
     }
 }
